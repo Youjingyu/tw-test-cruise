1637410894838f1fd6e56a6e235c7a64e8f8dcd4
Youjingyu <735284268@qq.com>
2019-02-23 11:45:09 +0800

    refactor: format
    格式化代码风格

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -26,28 +26,28 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  .header {
-    background-color: #fff;
-    text-align: center;
-    box-shadow: 0 1px 6px -2px rgba(32,33,36,0.28);
-    .logo {
-      margin: 10px auto;
-      height: 40px;
+.header {
+  background-color: #fff;
+  text-align: center;
+  box-shadow: 0 1px 6px -2px rgba(32, 33, 36, 0.28);
+  .logo {
+    margin: 10px auto;
+    height: 40px;
+  }
+  .login-status {
+    margin: 10px 0;
+    float: right;
+    margin-right: 10%;
+    img {
+      margin-right: 5px;
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+      vertical-align: middle;
     }
-    .login-status {
-      margin: 10px 0;
-      float: right;
-      margin-right: 10%;
-      img {
-        margin-right: 5px;
-        width: 30px;
-        height: 30px;
-        border-radius: 50%;
-        vertical-align: middle;
-      }
-      i {
-        color: rgb(133, 127, 127);
-      }
+    i {
+      color: rgb(133, 127, 127);
     }
   }
+}
 </style>
