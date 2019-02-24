9c02c57abef7ad0726ce31738d7c5a33e54a268d
Youjingyu <735284268@qq.com>
2019-02-24 16:13:50 +0800

    feat: view
    登录框

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,19 @@
 <template>
   <header class="header">
     <img class="logo" src="../assets/logo.svg" alt="logo">
-    <div class="login-status">
+    <div class="login" @click="toggleMenu">
       <img :src="avatarImgUrl || anonymousUser" alt="avatar">
-      <i v-if="isLogined" class="icon-angle-down"></i>
+      <i class="icon-angle-down"></i>
+      <ul class="menu" v-show="isShowMenu">
+        <li>
+          <i class="icon-id-card"></i>
+          Profile
+        </li>
+        <li>
+          <i class="icon-sign-in"></i>
+          Sign out
+        </li>
+      </ul>
     </div>
   </header>
 </template>
@@ -15,12 +25,18 @@ export default {
   name: 'Header',
   data () {
     return {
-      anonymousUser
+      anonymousUser,
+      isShowMenu: false
     }
   },
   props: {
     isLogined: Boolean,
     avatarImgUrl: String
+  },
+  methods: {
+    toggleMenu () {
+      this.isShowMenu = !this.isShowMenu
+    }
   }
 }
 </script>
@@ -30,27 +46,49 @@ export default {
 
 .header {
   height: $HeaderHeight;
+  line-height: $HeaderHeight;
   width: 100%;
   background-color: #fff;
   text-align: center;
   box-shadow: 0 1px 6px -2px rgba(32, 33, 36, 0.28);
   .logo {
-    margin: 10px auto;
-    height: 40px;
+    height: size(52);
+    vertical-align: middle;
   }
-  .login-status {
-    margin: 10px 0;
+  .login {
     float: right;
+    position: relative;
     margin-right: 10%;
     img {
       margin-right: 5px;
-      width: 30px;
-      height: 30px;
+      width: size(60);
+      height: size(60);
       border-radius: 50%;
       vertical-align: middle;
     }
     i {
       color: rgb(133, 127, 127);
+    }
+    .menu {
+      z-index: 9;
+      position: absolute;
+      top: size(90);
+      padding: size(18) 0;
+      width: size(180);
+      line-height: size(50);
+      box-shadow: 0 1px 6px -2px $Black;
+      background-color: $White;
+      li {
+        padding: 0 size(16);
+        text-align: left;
+        i {
+          display: inline-block;
+          width: size(40);
+        }
+        &:hover {
+          background-color: #EFEFEF;
+        }
+      }
     }
   }
 }
